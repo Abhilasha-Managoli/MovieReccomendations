248685e35f1c6f9f272b07189718c7e360263a01
Abhilasha <xap2xa@virginia.edu>
2024-06-29 18:01:35 -0400

    Customized home page

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'users'
 urlpatterns = [
-    path("", views.home),
+    path("", views.home, name="sign_in"),
     path("logout", views.logout_view)
 ]
