e86dbb804ea29b2dae9dc4f5e607aff2bab390cd
pratistha-dhungana <pratisthadh@gmail.com>
2024-07-05 20:29:48 -0400

    changed the aesthetic of the page.

--- users/views.py
+++ users/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
 
 def home(request):
     return render(request, "home.html")
 
+@login_required
+def movie_recommendation(request):
+    user_name = request.user.first_name.capitalize() if request.user.first_name else ''
+    return render(request, 'moviehome.html', {'user_name': user_name})
+
+@login_required
+def process_movie(request):
+    if request.method == 'POST':
+        favorite_movie = request.POST.get('favorite_movie')
+        return render(request, 'moviehome.html', {'user_name': request.user.first_name.capitalize(),
+                                                  'favorite_movie': favorite_movie})
+    # If the method is not POST, redirect to the movie_recommendation page
+    return redirect('users:movie_recommendation')
 
 def logout_view(request):
     logout(request)
