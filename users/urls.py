a7f810ec498623f43436bb77a2fcbf159174820e
Abhilasha <xap2xa@virginia.edu>
2024-07-07 17:46:54 -0400

    Connected API to the front end to display movie title, rating, and picture

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("movie_recommendation/", views.movie_recommendation, name='movie_recommendation'),
     path("process_movie/", views.process_movie, name='process_movie'),
-    path("logout/", views.logout_view, name='logout')
+    path("logout/", views.logout_view, name='logout'),
+    path('recommendations/', views.recommendations, name='recommendations'),
 ]
--- users/views.py
+++ users/views.py
@@ -8,10 +8,12 @@ from django.contrib.auth import logout
 def home(request):
     return render(request, "home.html")
 
+
 @login_required
 def movie_recommendation(request):
     user_name = request.user.first_name.capitalize() if request.user.first_name else ''
     return render(request, 'moviehome.html', {'user_name': user_name})
+
 
 @login_required
 def process_movie(request):
@@ -22,23 +24,25 @@ def process_movie(request):
     # If the method is not POST, redirect to the movie_recommendation page
     return redirect('users:movie_recommendation')
 
+
 def logout_view(request):
     logout(request)
     return redirect("/")
 
 
+@login_required
 def recommendations(request):
     query = request.GET.get('query', '')
-    url = 'https://api.themoviedb.org/3'
+    url = 'https://api.themoviedb.org/3/search/movie'
     params = {
         'api_key': "6d9d64446aa322b6e954111c63b34344",
-        'query': query
+        'query': query,
     }
     try:
         response = requests.get(url, params=params)
         response.raise_for_status()  # Check if the request was successful
         data = response.json()
         movies = data.get('results', [])
-        return render(request, 'moviehome/moviehome.html', {'movies': movies})
+        return render(request, 'moviehome.html', {'movies': movies, 'query': query})
     except requests.exceptions.RequestException as e:
         return HttpResponse(f"An error occurred: {e}", status=500)
