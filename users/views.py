c6033b65ab4797cff116343564c0563e33cd66fe
pratistha-dhungana <pratisthadh@gmail.com>
2024-07-08 02:43:48 -0400

    added a new feature called trending which shows current most watched movie.

--- users/views.py
+++ users/views.py
@@ -6,8 +6,19 @@ from django.contrib.auth import logout
 
 
 def home(request):
-    return render(request, "home.html")
+    url_trending = 'https://api.themoviedb.org/3/trending/movie/week'
+    params = {
+        'api_key': "6d9d64446aa322b6e954111c63b34344",
+    }
+    try:
+        response_trending = requests.get(url_trending, params=params)
+        response_trending.raise_for_status()
 
+        trending_data = response_trending.json().get('results', [])[:6]
+
+        return render(request, 'home.html', {'trending_movies': trending_data})
+    except requests.exceptions.RequestException as e:
+        return HttpResponse(f"An error occurred: {e}", status=500)
 
 @login_required
 def movie_recommendation(request):
