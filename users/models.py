1ec77c0b0208a2962289e0ed11e6164ae471891d
pratistha-dhungana <pratisthadh@gmail.com>
2024-07-26 16:47:25 -0400

    added a wishlist button on the homepage.

--- users/models.py
+++ users/models.py
@@ -2,9 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-
-
 class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     movie_title = models.CharField(max_length=255)
