042ba8f60bd8068e1db9d4ab7b6f08ae2ace0f04
pratistha-dhungana <pratisthadh@gmail.com>
2024-08-01 23:09:05 -0400

    made the logo a clickable button that brings you back to the homepage

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('', views.home, name="home"),
     path("movie_recommendation/", views.movie_recommendation, name='movie_recommendation'),
     path("process_movie/", views.process_movie, name='process_movie'),
     path("logout/", views.logout_view, name='logout'),
